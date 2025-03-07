18ac91b272afbd2242c43a23d690c520edabbdab
EmelyNj <23emelynajera@gmail.com>
2025-03-06 19:39:51 -0600

    Agregar archivos de proyecto.

---
+++ PracticaCodeDataFirts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaCodeDataFirts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
