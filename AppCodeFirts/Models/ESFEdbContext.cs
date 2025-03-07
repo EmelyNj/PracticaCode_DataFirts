ea0f692de5ff3a5e55e6f7489194fb086bded477
EmelyNj <23emelynajera@gmail.com>
2025-03-06 20:29:57 -0600

    Se creo la clase "ESFEdbContext" y se modifico la cadena de conexion.

---
+++ AppCodeFirts/Models/ESFEdbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppCodeFirts.Models
+{
+    public class ESFEdbContext:DbContext
+    {
+        public ESFEdbContext(DbContextOptions<ESFEdbContext> options) : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Alumno> Alumnos {  get; set; }
+    }
+}
