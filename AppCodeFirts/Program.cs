e4e5e501616c4d23f67b5d8baf923f38e34233a0
EmelyNj <23emelynajera@gmail.com>
2025-03-06 20:35:14 -0600

    Se modifico el archivo program.

--- AppCodeFirts/Program.cs
+++ AppCodeFirts/Program.cs
@@ -1,7 +1,16 @@
+using AppCodeFirts.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ESFEdbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
