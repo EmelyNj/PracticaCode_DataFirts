a97857a6a58f718dd0d330a3abd55dfda2b28d6e
EmelyNj <23emelynajera@gmail.com>
2025-03-06 19:46:14 -0600

    Se creo el proyecto "AppCodeFirts"

---
+++ AppCodeFirts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppCodeFirts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
